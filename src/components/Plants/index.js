2791a7db910cc690eb82feb55084ffb293a9368d
colin reilly <colinrlly@gmail.com>
2018-07-11 20:19:02 -0400

    Added automatic timezone detection for charts

--- src/components/Plants/index.js
+++ src/components/Plants/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import LineChart from './components/LineChart/index.js'
+import Moment from 'moment-timezone'
+
 
 class Plants extends Component {
     constructor(props) {
         super(props);
+
+        this.time_format = 'h:mm a';
 
         this.state = {
             data: {}
@@ -11,9 +15,21 @@ class Plants extends Component {
     }
 
     componentDidMount() {
-        fetch('/api/mvp_sensor_data')  // Make request
+        fetch('http://localhost:3001/api/mvp_sensor_data')  // Make request
             .then((res) => res.json())  // Parse promise
             .then((f_data) => {  // Do stuff with the retrieved data
+                // Format and localize the time labels
+                f_data['temp_times'] = f_data['temp_times']
+                    .map(x => Moment(x)
+                        .local()
+                        .tz(Moment.tz.guess())
+                        .format(this.time_format));
+                f_data['humid_times'] = f_data['humid_times']
+                    .map(x => Moment(x)
+                        .local()
+                        .tz(Moment.tz.guess())
+                        .format(this.time_format));
+
                 this.setState({
                     data: f_data
                 })
